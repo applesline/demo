b0b4c3e8e3041cdce8e2813124c330e9e44e1a9d
刘亚平 <liuyaping@hebao.im>
2017-12-22 14:56:16 +0800

    timingWheel demo

---
+++ timingWheel/ExpirationListener.java
@@ -0,0 +1,18 @@
+package org.liuyaping.demo.timingwheel;
+/** 
+ * A listener for expired object events. 
+ *  
+ * @author mindwind 
+ * @version 1.0, Sep 20, 2012 
+ * @see TimingWheel 
+ */  
+public interface ExpirationListener<E> {  
+      
+    /** 
+     * Invoking when a expired event occurs. 
+     *  
+     * @param expiredObject 
+     */  
+    void expired(E expiredObject);  
+      
+}  
